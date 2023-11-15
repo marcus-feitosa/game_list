00092e0c3d07a01050a6187161fe03c91239c17b
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-11-15 11:23:28 -0300

    GameRepository created

---
+++ src/main/java/com/marcusfeitosa/game_list/repository/GameRepository.java
@@ -0,0 +1,7 @@
+package com.marcusfeitosa.game_list.repository;
+
+import com.marcusfeitosa.game_list.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository <Game, Long> {
+}
