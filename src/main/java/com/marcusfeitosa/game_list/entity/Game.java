ca971d411707977c09671d72fb5fcaef3e684a91
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-11-15 11:06:05 -0300

    Game entity created

---
+++ src/main/java/com/marcusfeitosa/game_list/entity/Game.java
@@ -0,0 +1,25 @@
+package com.marcusfeitosa.game_list.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer gameYear;
+    private String gameGenre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
